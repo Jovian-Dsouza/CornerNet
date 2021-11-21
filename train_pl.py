583778116338f86b5be4861510bc56a886e9daad
Jovian Dsouza <dsouzajovian123@gmail.com>
2021-11-21 21:42:08 +0530

    ModelCheckpointing and auto_lr_find

--- train_pl.py
+++ train_pl.py
@@ -1,8 +1,10 @@
 import warnings
+
+from pytorch_lightning import callbacks
+from pytorch_lightning.core import datamodule
 warnings.filterwarnings("ignore")
 
 from pytorch_lightning import profiler
-from tensorboard import data
 import torch
 import pytorch_lightning as pl
 
@@ -11,10 +13,26 @@ from models.model import CornerNetPL
 from DataModule import DataModule
 import shutil
 import os
-import pickle
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+def lr_finder(trainer, model, datamodule):
+    '''
+    trainer = pl.Trainer(auto_lr_find=True)
+    '''
+    lr_finder = trainer.tuner.lr_find(model, datamodule=datamodule)
+    # fig = lr_finder.plot(suggest=True)
+    # fig.show()
+    model.model.hparams.lr = lr_finder.suggestion()
+    print('Best learning rate : ', model.model.hparams.lr)
 
 def main():
     pl.seed_everything(137, workers=True)
+    checkpoint_cb = ModelCheckpoint(
+        save_last=True,
+        monitor='val_loss',
+        mode='min',
+        filename='{epoch}-{val_loss:.4f}',
+    )
     datamodule = DataModule(
         root_dir="../VOC100examples",
         # spatial_resolution=[512, 512],
@@ -35,15 +53,17 @@ def main():
 
     trainer = pl.Trainer(
                         # profiler="advanced",
-                        fast_dev_run=False, 
+                        # fast_dev_run=False,
+                        auto_lr_find=True, 
                         max_epochs=1, 
                         precision=32,
                         benchmark=True,
                         gpus=-1,
                         # progress_bar_refresh_rate=20, #for colab
-                        limit_train_batches=1,
-                        # limit_val_batches=0,
+                        # limit_train_batches=1,
+                        # limit_val_batches=1,
                         # default_root_dir='..',
+                        callbacks=[checkpoint_cb],
                         )
     
     trainer.fit(model, datamodule=datamodule)
